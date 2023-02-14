857048bfdc341eda7484963c9024abf3234848a1
haresh94599520 <haresh0398@gmail.com>
2023-02-14 12:13:47 +0530

    add toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ function Header({ theme, setTheme }) {
   return (
     <div className="app-header">
       <h1>Notes List</h1>
-      <button className="theme-toggle" onClick={() => setTheme(!theme)}>
-        {theme ? "🌙" : "☀️"}
-      </button>
+      <button
+        className={"theme-toggle " + (theme ? "dark" : "")}
+        onClick={() => setTheme(!theme)}
+      ></button>
     </div>
   );
 }
